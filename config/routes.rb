0e21c9dfa96c5b49f62f49e3ef0f8f89e1dabd7d
Nick Lamb <nlamb@hotmail.co.uk>
2021-10-14 16:12:23 +0100

    fix links on organisations#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :organisations do
-    resources :facilities, only: [ :new, :create, :edit, :update]
+    resources :facilities, only: [ :new, :create, :edit ]
   end
-  resources :facilities, only: [ :index, :show, :destroy ]
+  resources :facilities, only: [ :index, :show, :update, :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
