22fa242df404bac44a7401b343d304dc367c3436
Nick Lamb <nlamb@hotmail.co.uk>
2021-11-01 21:19:23 +0000

    add search bar to navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if params[:query].present?
+      sql_query = "name ILIKE :query OR address ILIKE :query OR summary ILIKE :query"
+      @facilities = Facility.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @facilities = Facility.all
+    end
   end
 end
